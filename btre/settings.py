27aefefb61255a39afdfd6591d13e36450d98cf3
Chris <me@chrisconnelly.dev>
2022-01-28 05:02:02 -0700

    V 0.0.2
    Added Postgres DB
    Migrated Postgres Data
    Set Schemas 
    Created Listing Model 
    Created Realtor Model 
    Created Super User for admin
    registered models with admin 
    Added Media function
    Customized Admin Area 
    Added more data admin area

--- btre/settings.py
+++ btre/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': '1337wASSgob$',
+        'HOST': 'localhost'
     }
 }
 
@@ -127,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
